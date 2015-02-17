96c145a68a0fa478cdc0f451212ba067d902a496
progammer-rkt <rajeevphpdeveloper@gmail.com>
2015-02-17 11:54:08 +0530

    initial commit

---
+++ app/code/local/Rkt/DemoViewer/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+
+class Rkt_DemoViewer_IndexController extends Mage_Core_Controller_Front_Action
+{
+	public function indexAction()
+	{
+		$this->loadLayout();
+		$this->renderLayout();
+	}
+}
